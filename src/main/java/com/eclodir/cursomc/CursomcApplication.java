0b0665ecebf4692dc235fb7638e14fa9e7f3a12b
José Luiz <jose.gonzaga@eclodir.com>
2018-02-27 04:28:48 -0300

    Testado REST

--- src/main/java/com/eclodir/cursomc/CursomcApplication.java
+++ src/main/java/com/eclodir/cursomc/CursomcApplication.java
@@ -1,4 +1,4 @@
-package ocm.eclodir.cursomc;
+package com.eclodir.cursomc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
