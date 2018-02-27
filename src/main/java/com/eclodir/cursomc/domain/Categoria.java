f95dcc07ffd5ace608dc044d1979d5aa8a065c0f
José Luiz <jose.gonzaga@eclodir.com>
2018-02-27 06:54:13 -0300

    Implementado relacionamento ManyToMany entre Categoria e Produto

--- src/main/java/com/eclodir/cursomc/domain/Categoria.java
+++ src/main/java/com/eclodir/cursomc/domain/Categoria.java
@@ -1,11 +1,14 @@
 package com.eclodir.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable {
@@ -15,6 +18,9 @@ public class Categoria implements Serializable {
 	@GeneratedValue (strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	
+	@ManyToMany (mappedBy="categorias")
+	private List<Produto> produtos = new ArrayList<>();
 	
 	public Categoria() {}
 
@@ -32,6 +38,15 @@ public class Categoria implements Serializable {
 		this.id = id;
 	}
 
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
+	
+	
 	public String getNome() {
 		return nome;
 	}
@@ -64,7 +79,8 @@ public class Categoria implements Serializable {
 			return false;
 		return true;
 	}
-	
+
+
 	
 
 }
