5f739fcbbc1d5a5101dc768bed17583ee43c3c99
José Luiz <jose.gonzaga@eclodir.com>
2018-03-22 01:13:24 -0300

    Post de cliente

--- src/main/java/com/eclodir/cursomc/resources/ClienteResoucer.java
+++ src/main/java/com/eclodir/cursomc/resources/ClienteResoucer.java
@@ -1,5 +1,6 @@
 package com.eclodir.cursomc.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,9 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.eclodir.cursomc.domain.Cliente;
 import com.eclodir.cursomc.dto.ClienteDTO;
+import com.eclodir.cursomc.dto.ClienteNewDTO;
 import com.eclodir.cursomc.services.ClienteService;
 
 @RestController
@@ -66,5 +69,14 @@ public class ClienteResoucer {
 		return ResponseEntity.ok().body(listDTO);
 
 }
+	
+	@RequestMapping (method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDTO) {
+			
+			Cliente obj = service.fromDTO(objDTO);
+			obj = service.insert(obj);
+			URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+			return ResponseEntity.created(uri).build();	
+	}
 	
 }
--- src/main/java/com/eclodir/cursomc/services/ClienteService.java
+++ src/main/java/com/eclodir/cursomc/services/ClienteService.java
@@ -8,9 +8,15 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.eclodir.cursomc.domain.Cidade;
 import com.eclodir.cursomc.domain.Cliente;
+import com.eclodir.cursomc.domain.Endereco;
+import com.eclodir.cursomc.domain.enums.TipoCliente;
 import com.eclodir.cursomc.dto.ClienteDTO;
+import com.eclodir.cursomc.dto.ClienteNewDTO;
+import com.eclodir.cursomc.repositories.CidadeRepository;
 import com.eclodir.cursomc.repositories.ClienteRepository;
+import com.eclodir.cursomc.repositories.EnderecoRepository;
 import com.eclodir.cursomc.services.exceptions.DataIntegrityException;
 import com.eclodir.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -19,23 +25,29 @@ public class ClienteService {
 	
 	@Autowired
 	ClienteRepository repo;
-
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	
 	public Cliente find(Integer id) {
-		
 		Cliente cliente = repo.findOne(id);
-		
 		if (cliente == null) {
 			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", referente a classe "+ Cliente.class.getName());
 		}
 		return cliente;
 	}
 
-
 	public Cliente insert(Cliente obj) {
-		
 		obj.setId(null);
-		return repo.save(obj);
+		repo.save(obj);
+		enderecoRepository.save(obj.getEnderecos());
+		return obj;
+		
 	}
+
 
 	public Cliente update(Cliente obj) {
 		Cliente newObj = find(obj.getId());
@@ -64,6 +76,24 @@ public class ClienteService {
 	
 	public Cliente fromDTO(ClienteDTO objDTO) {
 		return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), null, null);
+	}
+	
+	public Cliente fromDTO(ClienteNewDTO objDTO) {
+		Cliente cli = new Cliente(null, objDTO.getNome(), objDTO.getEmail(), objDTO.getCpfOuCnpj(), TipoCliente.toEnum(objDTO.getTipo()));
+		Cidade cid = cidadeRepository.findOne(objDTO.getCidadeId());
+		Endereco end = new Endereco(null, objDTO.getLogradouro(), objDTO.getNumero(), objDTO.getComplemento(), objDTO.getBairro(), objDTO.getCep(), cli,cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDTO.getTelefone1());
+		if (objDTO.getTelefone2() != null) {
+			cli.getTelefones().add(objDTO.getTelefone2());
+		}
+		if (objDTO.getTelefone3() != null) {
+			cli.getTelefones().add(objDTO.getTelefone3());
+		}
+		
+		return cli;
+	
+		
 	}
 	
 	
