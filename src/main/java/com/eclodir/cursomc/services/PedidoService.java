8a155162dd5d733eaa2bee189c8a6dcca41e250e
José Luiz <jose.gonzaga@eclodir.com>
2018-04-04 09:57:28 -0300

    Implementando texto do email

--- src/main/java/com/eclodir/cursomc/services/PedidoService.java
+++ src/main/java/com/eclodir/cursomc/services/PedidoService.java
@@ -9,6 +9,7 @@ import com.eclodir.cursomc.domain.ItemPedido;
 import com.eclodir.cursomc.domain.PagamentoComBoleto;
 import com.eclodir.cursomc.domain.Pedido;
 import com.eclodir.cursomc.domain.enums.EstadoPagamento;
+import com.eclodir.cursomc.repositories.ClienteRepository;
 import com.eclodir.cursomc.repositories.ItemPedidoRepository;
 import com.eclodir.cursomc.repositories.PagamentoRepository;
 import com.eclodir.cursomc.repositories.PedidoRepository;
@@ -33,6 +34,9 @@ public class PedidoService {
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
 	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	public Pedido find(Integer id) {
 		Pedido obj = repo.findOne(id);
 		if (obj == null) {
@@ -45,6 +49,7 @@ public class PedidoService {
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
+		obj.setCliente(clienteRepository.findOne(obj.getCliente().getId()));
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
 		if (obj.getPagamento() instanceof PagamentoComBoleto) {
@@ -55,12 +60,15 @@ public class PedidoService {
 		pagamentoRepository.save(obj.getPagamento());
 		for (ItemPedido ip : obj.getItens()) {
 			ip.setDesconto(0.0);
-			ip.setPreco(produtoRepository.findOne(ip.getProduto().getId()).getPreco());
+			ip.setProduto(produtoRepository.findOne(ip.getProduto().getId()));
+			ip.setPreco(ip.getProduto().getPreco());
 			ip.setPedido(obj);
 
 		}
 		itemPedidoRepository.save(obj.getItens());
+		System.out.println(obj);
 		return obj;
+		
 	}
 	
 }
